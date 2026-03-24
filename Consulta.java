a6843a3e97e8ae80a08a717a920ca3037b580242
MarioCano <mcanmig@g.educaand.es>
2026-03-24 20:03:22 +0100

    Add files via upload

--- Consulta.java
+++ Consulta.java
@@ -1,12 +1,35 @@
 package ud6.veterinaria.v3;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Consulta {
 	private LocalDate fecha;
+	private int idAnimal;
+	private String diagnostico;
+	List<Medicamento> recetaMedicamento = new ArrayList<>();
 	
-	public Consulta(LocalDate fecha, int id, String medicamento) {
-		super(id, medicamento);
+	
+	public Consulta(LocalDate fecha, int idAnimal, String diagnostico) {
+		super();
 		this.fecha = fecha;
+		this.idAnimal = idAnimal;
+		this.diagnostico = diagnostico;
+		this.recetaMedicamento = recetaMedicamento;
 	}
+
+	public void addMedicamento(Medicamento m) {
+		this.recetaMedicamento.add(m);
+	}
+	
+	public boolean removeMedicamento(String nombreMedicamento) {
+		if(recetaMedicamento.contains(nombreMedicamento)) {
+			this.recetaMedicamento.remove(nombreMedicamento);
+			return true;
+		}
+		return false;
+		
+	}
+	
 }
