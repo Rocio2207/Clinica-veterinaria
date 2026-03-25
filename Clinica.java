d3e4918b97be62532e1f626fb7ed61ea23538d6f
Rocio2207 <rocio73gs@gmail.com>
2026-03-25 21:11:27 +0100

    Add files via upload

--- Clinica.java
+++ Clinica.java
@@ -3,10 +3,12 @@ package ud6.veterinaria.v3;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.LinkedHashSet;
+import java.util.Collections;
 
 public class Clinica {
 	
@@ -26,6 +28,10 @@ public class Clinica {
             this.animales.add(a);
     }
 	
+	public void addConsulta(int  id, Consulta c) {
+		
+	}
+	
 	
 	Set<Consulta> getConsultas(int idAnimal) throws AnimalNoEncontradoException{
 		if(consultas.containsKey(idAnimal)) {
@@ -36,8 +42,37 @@ public class Clinica {
 			throw new AnimalNoEncontradoException("El id proporcionado, no coincide con ningun animal.");
 		}
 		
-		
 	}
+	
+	public void mapAnimalesByPropietario() {
+	    Map<Propietario, List<Animal>> mapaPropietarios = new HashMap<>();
+	    for (Animal a : animales) {
+	        if (a.getPropietario() != null) {
+	            if (!mapaPropietarios.containsKey(a.getPropietario())) {
+	                mapaPropietarios.put(a.getPropietario(), new ArrayList<>());
+	            }
+	            mapaPropietarios.get(a.getPropietario()).add(a);
+	        }
+	    }
+	}
+	List <Medicamento> getMedicamentosByPeso(Consulta c){
+		List <Medicamento> medicamentoRec = new ArrayList<>(c.recetaMedicamento);
+		Collections.sort(medicamentoRec);
+		return medicamentoRec;		
+	}
+	
+	Set <Animal> getAnimalesSinConsultas(){
+		Set<Animal> listaAnimal = new HashSet<>();
+		for(Animal a : animales) {
+			if(!consultas.containsKey(a.getId())) {
+				listaAnimal.add(a);
+			}
+		}
+		return listaAnimal;
+	}
+
+	
+	
 	
 }
 
