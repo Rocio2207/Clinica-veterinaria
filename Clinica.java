6d208145f1dd03a61dcf8a6480ef321c80f2208b
prodrui1601 <prodrui1601@g.educaand.es>
2026-03-25 20:53:44 +0100

    Add files via upload

---
+++ Clinica.java
@@ -0,0 +1,56 @@
+package ud6.veterinaria.v3;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class Clinica {
+	
+	Map<Integer, List<Consulta>> consultas;
+	private List<Animal> animales = new ArrayList<>();
+	
+	Clinica(){
+		this.consultas = new HashMap<Integer, List<Consulta>>();
+		this.animales = new ArrayList<Animal>();
+	}
+	
+	public int cuantosAnimales() {
+		return Animal.getContadorAnimales();
+	}
+	
+	public void addAnimal(Animal a) {
+            this.animales.add(a);
+    }
+	
+	public void mapAnimalesByPropietario() {
+	    Map<Propietario, List<Animal>> mapaPropietarios = new HashMap<>();
+	    for (Animal a : animales) {
+	        if (a.getPropietario() != null) {
+	            if (!mapaPropietarios.containsKey(a.getPropietario())) {
+	                mapaPropietarios.put(a.getPropietario(), new ArrayList<>());
+	            }
+	            mapaPropietarios.get(a.getPropietario()).add(a);
+	        }
+	    }
+	}
+	Set<Consulta> getConsultas(int idAnimal) throws AnimalNoEncontradoException{
+		if(consultas.containsKey(idAnimal)) {
+			Set<Consulta> consultasId = new LinkedHashSet<Consulta>(consultas.get(idAnimal));
+			return consultasId;
+		}
+		else {
+			throw new AnimalNoEncontradoException("El id proporcionado, no coincide con ningun animal.");
+		}
+	}
+	
+}
+
+class AnimalNoEncontradoException extends Exception{
+	public AnimalNoEncontradoException(String mensaje) {
+		super(mensaje);
+	}
+}
